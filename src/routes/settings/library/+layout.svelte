95ed26c6e992fb9fe7b38bdb890f5c54ab94735e
MoldyTaint <MoldyTaint@no-reply.com>
2026-07-01 03:37:44 +0000

    feat(settings): add Library group layout with 5 tabs

---
+++ src/routes/settings/library/+layout.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { Library, FolderOpen, FileSignature, Shield, Filter } from 'lucide-svelte';
+	import { SettingsTabNav } from '$lib/components/settings';
+	import * as m from '$lib/paraglide/messages.js';
+
+	let { children } = $props();
+
+	const navItems = [
+		{
+			href: '/settings/library/libraries',
+			label: m.settings_general_tabLibraries(),
+			icon: Library
+		},
+		{
+			href: '/settings/library/root-folders',
+			label: m.settings_general_tabRootFolders(),
+			icon: FolderOpen
+		},
+		{
+			href: '/settings/library/naming',
+			label: m.nav_naming(),
+			icon: FileSignature
+		},
+		{
+			href: '/settings/library/quality',
+			label: m.nav_qualitySettings(),
+			icon: Shield
+		},
+		{
+			href: '/settings/library/filters',
+			label: m.nav_globalFilters(),
+			icon: Filter
+		}
+	];
+</script>
+
+<div class="flex min-h-full flex-col">
+	<SettingsTabNav {navItems} ariaLabel="Library settings" />
+
+	<div class="flex-1">
+		{@render children()}
+	</div>
+</div>
